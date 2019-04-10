90d8de2b388c44baf4a9cb72365ff37d9c22a015
Arkadiy <dragon_500@mail.ru>
2019-04-10 12:19:55 +0300

    Initial Commit

---
+++ Lab - Table App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab - Table App
+//
+//  Created by Arkadiy Grigoryanc on 10/04/2019.
+//  Copyright © 2019 Arkadiy Grigoryanc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
